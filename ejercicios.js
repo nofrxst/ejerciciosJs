da8ecdc7a05a07f652c7e06aed64778804e42176
harold Alcantara <harold_633@hotmail.com>
2020-02-15 15:16:25 -0500

    ejerccios js

---
+++ ejercicios.js
@@ -0,0 +1,30 @@
+// Ejercico 2
+/*
+let a  = prompt("ingrese numero 1");
+let b  = prompt("ingrese numero 2");
+let c = prompt("ingrese numero 3");
+
+function  Soc () {
+ 
+    a = Math.pow(a,3);
+    b = Math.pow(b,3);
+    c = Math.pow(c,3);
+
+    x = a + b +c ;
+    
+    return console.log( "Cubos individuales :" + a , b , c + " Suma de estos :" + x);
+    
+    
+
+}
+*/
+
+// Ejercicio 3 
+
+let x = prompt("Inserte valor");
+
+function valor (){
+   x = typeof(x)
+
+    return console.log(x)
+}
