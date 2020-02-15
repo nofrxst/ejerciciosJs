24da9cc8e2c6ab7c68b3e91641592ca6a4c68f57
harold Alcantara <harold_633@hotmail.com>
2020-02-15 12:03:23 -0500

    Ejercicios JS

---
+++ main.js
@@ -0,0 +1,97 @@
+//debugger;
+// Arrays
+/*
+  let numbers = [3, 2, 4, 1, 5, 10];
+let total = 0;
+for( let index = 0 ; index < numbers.length ; index = index + 1  ) {
+    total = total + numbers [index];
+}
+
+console.log("total ", total);
+*/
+
+let n1 = 13 ;
+
+let n2 = 4;
+
+sumar = n1 + n2 
+
+console.log("E1 - La suma es", sumar);
+
+// Ejercicio 2 
+
+let a = 12;
+let b = 13;
+let c = 14;
+let d = 08;
+
+promedio = (a + b + c +d )/4
+
+console.log("E2 - El promedio es :", promedio);
+
+// ejercicio 3 
+
+let base = 5 ;
+let altura = 3;
+
+Ar = base * altura ;
+
+console.log("E3 - Area de un rectangulo : ", Ar);
+
+
+// calcular area de un triangulo 
+
+let ba = 9;
+let alt = 15 ;
+
+At = (ba*alt)/2
+console.log("E4 - Area de un triangulo : ", At);
+
+// ejercicio 5 area de una circunferencia 
+
+let radio = 6;
+
+Ac =  radio*13.1416
+console.log("E5 - Area de un circunferencia : ", Ac);
+
+
+// ejercicio 6  sueldo
+
+let h = 8;
+let sh = 100;
+
+sueldo=((h*sh)*7);
+
+console.log("E6 - sueldo por semana es : " , sueldo);
+
+
+// ejercicio 7 metros a pulgadas 
+
+let metros = 4;
+
+pulgadas = metros / 0.0254 ;
+
+console.log("E7 - metros a pulgadas es : " , pulgadas);
+
+
+// ejercicio 8 soles a dolares
+
+let soles = 60;
+
+dolares = soles*0.30;
+
+console.log("E8 - soles a dolares es : ", dolares);
+
+
+// ejercicio 9 Edad
+
+
+let año_nac = 1997;
+
+edad = 2020 - año_nac;
+
+console.log("E9 - Edad es :" , edad);
+
+// Ejercicio 10 
+
+
